b9358245aa250df9f55bab870b8ad8c54e12947b
erenbagci <ereenbagci@gmail.com>
2021-10-03 13:00:37 -0700

    fiesta resim değiştirme

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    var isFiesta = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Merhaba dünya!")
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
+    @objc func changePic(){
+        if isFiesta == true{
+        imageView.image = UIImage(named: "fiesta2")
+            myLabel.text = "fiesta2"
+            isFiesta = false
+        } else {
+            imageView.image = UIImage(named: "Fiesta")
+            myLabel.text = "fiesta"
+            isFiesta = true
+            
+        }
+    }
+    
 }
 
